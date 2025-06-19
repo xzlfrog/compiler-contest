005e688241fc7d5e5b17d1cd0e589cbe8d0f3a3a
xzl_frog <114553117+xzlfrog@users.noreply.github.com>
2025-06-19 21:24:18 +0800

    Update sym.hpp

--- include/sym.hpp
+++ include/sym.hpp
@@ -1,12 +1,10 @@
 #pragma once
 
-#include<string>
-#include<variant>
-#include<stdexcept>
-#include<vector>
-#include<utility>
+#include"data.hpp"
 
-using ValueVariant = std::variant<bool,char,short,int,long long,float,double,void*>;
+#define GLOBAL_SCOPE 0
+#define FUNCTION_SCOPE 1
+#define BLOCK_SCOPE 2
 
 enum symType{
     sym_undefined,
@@ -19,261 +17,131 @@ enum symType{
     constant_i32,
     constant_i64,
     constant_f32,
-    constant_f64
-};
-
-enum dataType{
-    data_undefined,
-    void_,
-    i1,
-    i8,
-    i16,
-    i32,
-    i64,
-    f32,
-    f64,
+    constant_f64,
+    array,
     pointer
 };
 
-class Data {
-private:
-    dataType type;
-    
-public:
-    virtual dataType getType() const =0;
-    virtual ValueVariant getValue() const =0;
-    virtual void setValue(ValueVariant value) =0;
-};
-
-class Data_i1:public Data {
-private:
-    bool value;
-public:
-    dataType getType() const override {
-        return dataType::i1;
-    }
-    
-    ValueVariant getValue() const override {
-        return value;
-    }
-    
-    void setValue(ValueVariant value) override {
-        if (std::holds_alternative<bool>(value)) {
-            this->value = std::get<bool>(value);
-        } else {
-            throw std::invalid_argument("ValueVariant is not bool type");
-        }
-    }
-};
-
-class Data_i8:public Data {
-private:
-    char value;
-public:    
-    dataType getType() const override {
-        return dataType::i8;
-    }
-    
-    ValueVariant getValue() const override {
-        return value;
-    }
-    
-    void setValue(ValueVariant value) override {
-        if (std::holds_alternative<char>(value)) {
-            this->value = std::get<char>(value);
-        } else {
-            throw std::invalid_argument("ValueVariant is not char type");
-        }
-    }
-};
-
-class Data_i16:public Data {
-private:
-    short value;
-public:    
-    dataType getType() const override {
-        return dataType::i16;
-    }
-    
-    ValueVariant getValue() const override {
-        return value;
-    }
-    
-    void setValue(ValueVariant value) override {
-        if (std::holds_alternative<short>(value)) {
-            this->value = std::get<short>(value);
-        } else {
-            throw std::invalid_argument("ValueVariant is not short type");
-        }
-    }
-};
-
-class Data_i32:public Data {
-private:
-    int value;
-public:    
-    dataType getType() const override {
-        return dataType::i32;
-    }
-    
-    ValueVariant getValue() const override {
-        return value;
-    }
-    
-    void setValue(ValueVariant value) override {
-        if (std::holds_alternative<int>(value)) {
-            this->value = std::get<int>(value);
-        } else {
-            throw std::invalid_argument("ValueVariant is not int type");
-        }
-    }
-};
-
-class Data_i64:public Data {
-private:
-    long long value;
-public:    
-    dataType getType() const override {
-        return dataType::i64;
-    }
-    
-    ValueVariant getValue() const override {
-        return value;
-    }
-    
-    void setValue(ValueVariant value) override {
-        if (std::holds_alternative<long long>(value)) {
-            this->value = std::get<long long>(value);
-        } else {
-            throw std::invalid_argument("ValueVariant is not long long type");
-        }
-    }
-};
-
-class Data_f32:public Data {
-private:
-    float value;
-public:    
-    dataType getType() const override {
-        return dataType::f32;
-    }
-    
-    ValueVariant getValue() const override {
-        return value;
-    }
-    
-    void setValue(ValueVariant value) override {
-        if (std::holds_alternative<float>(value)) {
-            this->value = std::get<float>(value);
-        } else {
-            throw std::invalid_argument("ValueVariant is not float type");
-        }
-    }
-};
-
-class Data_f64:public Data {
-private:
-    double value;
-public:    
-    dataType getType() const override {
-        return dataType::f64;
-    }
-    
-    ValueVariant getValue() const override {
-        return value;
-    }
-    
-    void setValue(ValueVariant value) override {
-        if (std::holds_alternative<double>(value)) {
-            this->value = std::get<double>(value);
-        } else {
-            throw std::invalid_argument("ValueVariant is not double type");
-        }
-    }
-};
-
-class Data_pointer:public Data {
-private:
-    void* value;
-public:
-    dataType getType() const override {
-        return dataType::pointer;
-    }
-    
-    ValueVariant getValue() const override {
-        return value;
-    }
-    
-    void setValue(ValueVariant value) override {
-        if (std::holds_alternative<void*>(value)) {
-            this->value = std::get<void*>(value);
-        } else {
-            throw std::invalid_argument("ValueVariant is not pointer type");
-        }
-    }
-};
-
 class Symbol {
-private:
-    //只考虑了指针，数组，基本数据类型，此处未考虑指针的指针，函数指针，指针数组的情况，若需要该功能，此处需要修改
-    dataType elementType;//当前指针指向的元素类型
-    std::vector<int> dimensions; //数组的维度信息 
-    Symbol* pointedElement;//指向的符号
-
 public:
-    std::string name;
     symType type;
+    std::string name;
     Data* data;
     int offset;
     int scope;
     Symbol* next;
     bool isArray;//是否是数组
 
-    Symbol(const std::string& name,symType type,Data* data)
-        : name(name), type(type), data(data), offset(0), scope(0), next(nullptr) {}
+    virtual symType getType() const;
     
     Symbol(const std::string& name)
         : name(name), type(symType::sym_undefined) , data(nullptr), offset(0), scope(0), next(nullptr) {}
 
     Symbol(const std::string& name,symType type)
-        : name(name), type(symType::sym_undefined) , data(nullptr), offset(0), scope(0), next(nullptr) {}
+        : name(name), type(type) , data(nullptr), offset(0), scope(0), next(nullptr) {}
 
-    Symbol(const std::string& name,Data*data)
-        : name(name), type(symType::sym_undefined) , data(data), offset(0), scope(0), next(nullptr){}
+};
 
+//只考虑了指针的情况，未考虑指针的指针的情况，如需使用需后续补充
+class PointerSymbol:public Symbol{
+private:
+    dataType PointedType;//当前指针指向的元素类型
 
-    //yacc文件中不需要调用以下方法
-    //----------------------------------------------------------------
-    //在allocaLLVM类中调用，yacc文件中调用allocaLLVM中的方法，不调用这个方法
-    template<typename... Args>
-    void allocateMemory(dataType elementType,Args... dims){
-        this->elementType = elementType;
-        Data_pointer* data_pointer = new Data_pointer();
-        data_pointer->setValue(nullptr);
-        this->data=data_pointer;
-        this->addDimensions(dims...);
+public:
+    Data* & pointedData;//指向的数据
+
+    using Symbol::Symbol;
+
+    symType getType() const override{return symType::pointer;}
+
+    dataType getPointedType() const {
+        return this->PointedType;
     }
+
+    void allocateMemory(dataType elementType,ValueVariant value=nullptr){
+        this->PointedType = elementType;
+
+        Data_pointer* data_pointer = new Data_pointer();
+        Data* data=createData(elementType,value);
+        this->data=new Data_pointer();
+        this->data->setValue(data);
+
+        this->pointedData=std::get<Data*>(this->data->getValue());
+    }
+
+};
+
+//包含常数符号和放在寄存器里的符号
+class BasicSymbol: public Symbol{
+public:
+    using Symbol::Symbol;
+
+    symType getType() const override{return this->type;}
+
+    dataType getDataType(){return this->data->getType();}
+
+    void setData(dataType dtype,ValueVariant v=nullptr){this->data=createData(dtype,v);}
+
+    void setType(symType type) {
+        if(type<symType::variable||type>symType::constant_f64)
+            throw std::invalid_argument("basic symbol type is wrong");
+        this->type=type;
+    }
+};
+
+//数组变量的符号,似乎存数组中的数据没有什么意义？除非在做优化的时候可以用到？
+class ArraySymbol: public Symbol{
+private:
+    std::vector<int> dimensions; //数组的维度信息 
+    dataType arrayType;
+    std::vector<int> elementNums;//比如数组a[5][6][7],则该向量中包含5*6*7,6*7,7,1表示每一个维度的"1"有几个元素
 
     template<typename... Args>
         void addDimensions(Args... dims){ 
         (this->dimensions.push_back(static_cast<size_t>(dims)), ...);
     }
 
-    void setPointedElement(Symbol* element){this->pointedElement=element;}
+public:
+    using Symbol::Symbol;
 
-    void addDimensions(){}
-    
-    void allocateRegister();
+    symType getType() const override{return symType::array;}
 
-    //从内存中加在数据到寄存器中
-    Symbol* loadFromMemory();
+    //如array.allocateMemory(dataType::i32,5,6,7)表示分配了一个i32 a[5][6][7]的数组
+    template<typename... Args>
+    void allocateMemory(dataType type,Args... dims){
+        this->arrayType=type;
+        this->addDimensions(dims...);
+        int size=1;
 
-    dataType getElementType() const {
-        return elementType;
+        for(auto dim:this->dimensions)
+            size=dim*size;
+        
+        this->elementNums.push_back(size);
+        for(auto dim:this->dimensions)
+            size=size/dim,this->elementNums.push_back(size);
     }
 
-    const std::vector<int>& getDimensions() const {
-        return dimensions;
-    }
+    std::vector<int>getDimensions(){return this->dimensions;}
 
+    dataType getArrayType(){return this->arrayType;}
+};
+
+
+//标签对应的符号
+class LabelSymbol:public Symbol{
+public:
+    using Symbol::Symbol;
+
+    symType getType() const override{return symType::label;}
+};
+
+class FuncSymbol:public Symbol{
+public:
+    std::vector<dataType> argumentsType;
+
+    using Symbol::Symbol;
+    symType getType() const override{return symType::function;}
+    void addArguments(std::vector<dataType> arguments);
+    void addArgument(dataType argument);
+    const std::vector<dataType> getArgumentsType()const{return this->argumentsType;}
 };
