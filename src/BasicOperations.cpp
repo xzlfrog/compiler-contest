e4d2363e3a779a44fbb805c4b254ae7d15d81343
xzl_frog <114553117+xzlfrog@users.noreply.github.com>
2025-06-19 21:25:53 +0800

    Update BasicOperations.cpp

--- src/BasicOperations.cpp
+++ src/BasicOperations.cpp
@@ -3,18 +3,18 @@
 #include "../include/llvm.hpp"
 #include "../include/function.hpp"
 
-void ArithmeticOperationLLVM::setOperandA(Symbol* operand)//set the result operand
+void ArithmeticOperationLLVM::setOperandA(BasicSymbol* operand)//set the result operand
     {
-        if(this->ty!=dataType::data_undefined){
+        if(this->llvmType>=icmp_eq&&this->llvmType<=fcmp_ord)
+            checkType(operand->data->getType(),dataType::i1);
+        else if(this->ty!=dataType::data_undefined)
             checkType(operand->data->getType(),this->ty);
-        }
-        else{
+        else
             this->ty=operand->data->getType();
-        }
         this->a=operand;
     }
 
-void ArithmeticOperationLLVM::setOperandB(Symbol* operand)//set the first operand
+void ArithmeticOperationLLVM::setOperandB(BasicSymbol* operand)//set the first operand
     {
         if(this->ty!=dataType::data_undefined){
             checkType(operand->data->getType(),this->ty);
@@ -25,7 +25,7 @@ void ArithmeticOperationLLVM::setOperandB(Symbol* operand)//set the first operan
         this->b=operand;
     }
 
-void ArithmeticOperationLLVM::setOperandC(Symbol* operand)//set the first operand
+void ArithmeticOperationLLVM::setOperandC(BasicSymbol* operand)//set the first operand
     {
         if(this->ty!=dataType::data_undefined){
             checkType(operand->data->getType(),this->ty);
@@ -44,7 +44,7 @@ void ArithmeticOperationLLVM::setLLVMType(LLVMtype type){
         }
     }
 
-void ArithmeticOperationLLVM::setOperand(Symbol* a, Symbol* b, Symbol *c) {
+void ArithmeticOperationLLVM::setOperand(BasicSymbol* a, BasicSymbol* b, BasicSymbol *c) {
     setOperandA(a);
     setOperandB(b);
     setOperandC(c);
