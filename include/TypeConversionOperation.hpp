3321a0584e769dcf7682f44d3cda24ba839d7196
xzl_frog <114553117+xzlfrog@users.noreply.github.com>
2025-06-19 21:25:02 +0800

    Update TypeConversionOperation.hpp

--- include/TypeConversionOperation.hpp
+++ include/TypeConversionOperation.hpp
@@ -1,22 +1,17 @@
+#pragma once
+
 #include"llvm.hpp"
 #include"function.hpp"
 
 //src_ty src_sym->dest_ty dest_sym
 class TypeConversionOperation:public LLVM{
 public:
-    Symbol* src_sym;
-    dataType src_ty;
-    Symbol* dest_sym;
-    dataType dest_ty;
+    BasicSymbol* src_sym;
+    BasicSymbol* dest_sym;
 
-    TypeConversionOperation(LLVMtype type,Symbol* src_sym=nullptr,dataType src_ty=dataType::data_undefined
-    ,Symbol* dest_sym=nullptr,dataType dest_ty=dataType::data_undefined):
-    src_sym(src_sym),src_ty(src_ty),dest_sym(dest_sym),dest_ty(dest_ty)
-    {
-        this->llvmType=type;
-        if(type<trunc||type>inttoptr)
-            throw std::invalid_argument("llvm type is invalid");
-        checkType(src_sym->data->getType(),src_ty);
-        checkType(dest_sym->data->getType(),dest_ty);
-    }
+    std::string out_str() const override;
+    BasicSymbol* getSrcSymbol(){return this->src_sym;}
+    BasicSymbol* getDestSymbol(){return this->dest_sym;}
+    dataType getSrcType(){return this->src_sym->getDataType();}
+    dataType getDestType(){return this->dest_sym->getDataType();}
 };
