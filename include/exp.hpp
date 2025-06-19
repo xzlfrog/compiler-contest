77ca86b6dbce8722e215ef263ce335c0e7a6d4d8
xzl_frog <114553117+xzlfrog@users.noreply.github.com>
2025-06-19 21:23:06 +0800

    Update exp.hpp

--- include/exp.hpp
+++ include/exp.hpp
@@ -1,23 +1,15 @@
 #pragma once
-
 #include"sym.hpp"
 #include"llvm.hpp"
 
-Symbol* local_sym_table;
-Symbol* global_sym_table;
+class Expression{
+public:
+    Expression* next;
+    LLVM* llvm; 
+    Symbol* sym;
 
-int tmp_symbol_count=0;
+    Expression(Expression* next,LLVM*llvm,Symbol*sym):
+    next(next),llvm(llvm),sym(sym){}
+    ;
+};
 
-void checkType(dataType type1,dataType type2);//类型检查在设置操作数的时候进行，不需要在yacc文件中使用
-
-void initial_program();
-
-std::string generate_variable_name();
-
-Symbol* get_tmp_variable(dataType type,int scope);
-
-Symbol* get_tmp_label();
-
-int getDataSize(dataType type);
-
-std::string DataTypeToString(dataType type);
