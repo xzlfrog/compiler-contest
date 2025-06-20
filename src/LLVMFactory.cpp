32258e180fbee91f5a98f4cda36d3c40af7f67f5
xzl_frog <114553117+xzlfrog@users.noreply.github.com>
2025-06-20 20:51:15 +0800

    Update LLVMFactory.cpp

--- src/LLVMFactory.cpp
+++ src/LLVMFactory.cpp
@@ -1,4 +1,5 @@
 #include "../include/LLVMFactory.hpp"
+#include"../include/SymbolFactory.hpp"
 
 ArithmeticOperationLLVM* LLVMfactory::createBasicOperationLLVM(LLVMtype type,BasicSymbol*a,BasicSymbol*b,BasicSymbol*c){
     ArithmeticOperationLLVM* basicOperationLLVM =new ArithmeticOperationLLVM();
@@ -73,7 +74,16 @@ AllocaNonArrayLLVM* LLVMfactory::createAllocaNonArrayLLVM(PointerSymbol* sym){
     AllocaNonArrayLLVM* allocaNonArrayLLVM=new AllocaNonArrayLLVM();
     allocaNonArrayLLVM->sym=sym;
     allocaNonArrayLLVM->llvmType=LLVMtype::allocate_nonarray;
+    allocaNonArrayLLVM->sym->data->setIsInitialize(false);
     return allocaNonArrayLLVM;
+}
+
+AllocaArrayLLVM* LLVMfactory::createAllocaArrayLLVM(ArraySymbol* sym,dataType type,std::vector<int>&dimensions){
+    AllocaArrayLLVM* allocaArrayLLVM=new AllocaArrayLLVM();
+    allocaArrayLLVM->array=sym;
+    allocaArrayLLVM->llvmType=LLVMtype::allocate_array;
+    allocaArrayLLVM->array->allocateMemory(type,dimensions);
+    return allocaArrayLLVM;
 }
 
 AllocaArrayLLVM* LLVMfactory::createAllocaArrayLLVM(ArraySymbol* sym){
@@ -99,11 +109,168 @@ StoreLLVM* LLVMfactory::createStoreLLVM(BasicSymbol* src_sym,PointerSymbol* dest
     return storeLLVM;
 }
 
-GetElementPtrLLVM* LLVMfactory::createGetElementPtrLLVM(ArraySymbol* ptrval,std::vector<dataType>type,std::vector<BasicSymbol*>idx){
+GetElementPtrLLVM* LLVMfactory::createGetElementPtrLLVM(BasicSymbol*dest_sym,ArraySymbol* ptrval,std::vector<dataType>type,std::vector<BasicSymbol*>idx){
     GetElementPtrLLVM* getElementPtrLLVM=new GetElementPtrLLVM();
     getElementPtrLLVM->ptrval=ptrval;
     getElementPtrLLVM->addTyIdx(type,idx);
     getElementPtrLLVM->llvmType=LLVMtype::getelementptr;
+    getElementPtrLLVM->ptrval=ptrval;
+    getElementPtrLLVM->dest_sym=dest_sym;
     return getElementPtrLLVM;
 }
 
+GlobalNonArrayVarDefination* LLVMfactory::createGlobalNonArrayVarDefination(PointerSymbol* dest_sym,BasicSymbol* src_sym){
+    GlobalNonArrayVarDefination* globalNonArrayVarDefination=new GlobalNonArrayVarDefination();
+    globalNonArrayVarDefination->dest_sym=dest_sym;
+    globalNonArrayVarDefination->src_sym=src_sym;
+    globalNonArrayVarDefination->llvmType=LLVMtype::global_nonarray;
+    globalNonArrayVarDefination->dest_sym->data=globalNonArrayVarDefination->src_sym->data;
+    return globalNonArrayVarDefination;
+ }
+
+GlobalNonArrayVarDefination* LLVMfactory::createGlobalNonArrayVarDefination(PointerSymbol* dest_sym,initializer initMode){
+    GlobalNonArrayVarDefination* globalNonArrayVarDefination=LLVMfactory::createGlobalNonArrayVarDefination(dest_sym,nullptr);
+    globalNonArrayVarDefination->dest_sym->pointedData->setInitMode(initMode);
+    if(initMode==initializer::undef)
+        globalNonArrayVarDefination->dest_sym->pointedData->setIsInitialize(false);
+    else if(initMode==initializer::zeroinitializer){
+        globalNonArrayVarDefination->dest_sym->pointedData->setValue(0);
+        globalNonArrayVarDefination->dest_sym->pointedData->setIsInitialize(true);
+    }
+    return globalNonArrayVarDefination;
+}
+
+static CallLLVM* createCallLLVM(FuncSymbol* func,std::vector<BasicSymbol*>& params){
+    CallLLVM* callLLVM=new CallLLVM();
+    callLLVM->addArguments(params);
+    callLLVM->function=func;
+    callLLVM->llvmType=LLVMtype::call;
+    dataType arg_ty1,arg_ty2;
+    for(int i=0;i<callLLVM->arguments.size();i++)
+    {
+        arg_ty1=callLLVM->arguments[i]->data->getType();
+        arg_ty2=func->paramTypes[i];
+        if(arg_ty1!=arg_ty2)
+            throw std::invalid_argument("function parameters type not match");
+    }
+    return callLLVM;
+}
+
+ConstantNonArrayVarDefination* LLVMfactory::createConstantNonArrayVarDefination(BasicSymbol* dest_sym,BasicSymbol* src_sym){
+    ConstantNonArrayVarDefination* constantNonArrayVarDefination=new ConstantNonArrayVarDefination();
+    constantNonArrayVarDefination->dest_sym=dest_sym;
+    constantNonArrayVarDefination->src_sym=src_sym;
+    constantNonArrayVarDefination->dest_sym->data=constantNonArrayVarDefination->src_sym->data;
+    constantNonArrayVarDefination->llvmType=LLVMtype::const_nonarray;
+    switch (constantNonArrayVarDefination->dest_sym->getDataType())
+    {
+    case i1:
+        constantNonArrayVarDefination->dest_sym->type=symType::constant_i1;
+        break;
+    case i8:
+        constantNonArrayVarDefination->dest_sym->type=symType::constant_i8;
+        break;
+    case i16:
+        constantNonArrayVarDefination->dest_sym->type=symType::constant_i16;
+        break;
+    case i32:
+        constantNonArrayVarDefination->dest_sym->type=symType::constant_i32;
+        break;
+    case i64:
+        constantNonArrayVarDefination->dest_sym->type=symType::constant_i64;
+        break;
+    case f32:
+        constantNonArrayVarDefination->dest_sym->type=symType::constant_f32;
+        break;
+    case f64:
+        constantNonArrayVarDefination->dest_sym->type=symType::constant_f64;
+        break;
+    default:
+        throw std::invalid_argument("constant type is wrong");
+        break;
+    }
+    return constantNonArrayVarDefination;
+}
+
+ConstantNonArrayVarDefination* LLVMfactory::createConstantNonArrayVarDefination(BasicSymbol* dest_sym,initializer initMode){
+    ConstantNonArrayVarDefination* constantNonArrayVarDefination=new ConstantNonArrayVarDefination();
+    constantNonArrayVarDefination->dest_sym=dest_sym;
+    constantNonArrayVarDefination->dest_sym->data->setInitMode(initMode);
+    constantNonArrayVarDefination->llvmType=LLVMtype::const_nonarray;
+    switch (constantNonArrayVarDefination->dest_sym->getDataType())
+    {
+    case i1:
+        constantNonArrayVarDefination->dest_sym->type=symType::constant_i1;
+        break;
+    case i8:
+        constantNonArrayVarDefination->dest_sym->type=symType::constant_i8;
+        break;
+    case i16:
+        constantNonArrayVarDefination->dest_sym->type=symType::constant_i16;
+        break;
+    case i32:
+        constantNonArrayVarDefination->dest_sym->type=symType::constant_i32;
+        break;
+    case i64:
+        constantNonArrayVarDefination->dest_sym->type=symType::constant_i64;
+        break;
+    case f32:
+        constantNonArrayVarDefination->dest_sym->type=symType::constant_f32;
+        break;
+    case f64:
+        constantNonArrayVarDefination->dest_sym->type=symType::constant_f64;
+        break;
+    default:
+        throw std::invalid_argument("constant type is wrong");
+        break;
+    }
+    if(initMode==initializer::undef)
+        constantNonArrayVarDefination->dest_sym->data->setIsInitialize(false);
+    else if(initMode==initializer::zeroinitializer){
+        constantNonArrayVarDefination->dest_sym->data->setValue(0);
+        constantNonArrayVarDefination->dest_sym->data->setIsInitialize(true);
+    }
+    return constantNonArrayVarDefination;
+}
+
+FuncDeclaration* createFuncDeclaration(FuncSymbol* func,std::vector<dataType>paramTypes,dataType returnType){
+    FuncDeclaration* funcDeclaration=LLVMfactory::createFuncDeclaration(func,paramTypes);
+    funcDeclaration->func->returnType=returnType;
+    return funcDeclaration;
+}
+
+FuncDeclaration* createFuncDeclaration(FuncSymbol* func,std::vector<dataType>paramTypes){
+    FuncDeclaration* funcDeclaration = new FuncDeclaration();
+    funcDeclaration->func=func;
+    funcDeclaration->func->paramTypes=paramTypes;
+    funcDeclaration->llvmType=LLVMtype::func_decl;
+    return funcDeclaration;
+}
+
+FuncDeclaration* LLVMfactory::createFuncDeclaration(FuncSymbol* func){
+    FuncDeclaration* funcDeclaration = new FuncDeclaration();
+    funcDeclaration->func=func;
+    funcDeclaration->llvmType=LLVMtype::func_decl;
+    return funcDeclaration;
+}
+
+FuncDefination* LLVMfactory::createFuncDefination(FuncSymbol* func,std::vector<BasicSymbol*>param){
+    FuncDefination* funcDefination = new FuncDefination();
+    funcDefination->func=func;
+    funcDefination->llvmType=LLVMtype::func_def;
+    funcDefination->params=param;
+    return funcDefination;
+}
+
+FuncDefination* LLVMfactory::createFuncDefination(FuncSymbol* func,std::vector<dataType>paramTypes,dataType returnType,std::vector<BasicSymbol*>param){
+    FuncDefination* funcDefination=LLVMfactory::createFuncDefination(func,param);
+    funcDefination->func->paramTypes=paramTypes;
+    funcDefination->func->returnType=returnType;
+    return funcDefination;
+}
+
+FuncDefination* LLVMfactory::createFuncDefination(FuncSymbol* func,std::vector<dataType>paramTypes,std::vector<BasicSymbol*>param){
+    FuncDefination* funcDefination=LLVMfactory::createFuncDefination(func,param);
+    funcDefination->func->paramTypes=paramTypes;
+    return funcDefination;
+}
