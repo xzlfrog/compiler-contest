62fe0f515f60092723cfcfe1f72c839d9c5e9fb3
xzl_frog <114553117+xzlfrog@users.noreply.github.com>
2025-06-20 20:43:31 +0800

    Update ControlOperations.hpp

--- include/ControlOperations.hpp
+++ include/ControlOperations.hpp
@@ -8,7 +8,7 @@ class ControlOperationLLVM:public LLVM{
 
 };
 
-//
+
 class ConditionalBranchLLVM:public ControlOperationLLVM{
 public:
     BasicSymbol* condition; // The condition for the branch
@@ -16,16 +16,12 @@ public:
     LabelSymbol* falseBranch; // The LLVM IR for the false branch
 
     std::string out_str() const override; // Output the LLVM IR string representation
-
     void setCondition(BasicSymbol* cond); // Set the condition for the branch
-
     void setTrueBranch(LabelSymbol* trueBranch); // Set the true branch LLVM IR
-
     void setFalseBranch(LabelSymbol* falseBranch); // Set the false branch LLVM IR
-
-    BasicSymbol* getCondition(){return this->condition;}
-    LabelSymbol* getTrueBranch(){return this->trueBranch;}
-    LabelSymbol* getFalseBranch(){return this->falseBranch;}
+    BasicSymbol* getCondition();
+    LabelSymbol* getTrueBranch();
+    LabelSymbol* getFalseBranch();
 };
 
 //br target
@@ -34,10 +30,8 @@ public:
     LabelSymbol* target; // The target label for the branch
 
     std::string out_str() const override; // Output the LLVM IR string representation
-
     void setTarget(LabelSymbol* target); // Set the target label for the branch
-
-    LabelSymbol* getTarget(){return this->target;}
+    LabelSymbol* getTarget();
 };
 
 //return 
@@ -46,39 +40,26 @@ public:
     BasicSymbol* returnValue; // The value to return
 
     std::string out_str() const override; // Output the LLVM IR string representation
-
     void setReturnValue(BasicSymbol* value); // Set the return value
-    BasicSymbol* getReturnValue(){return this->returnValue;}
-    dataType getReturnType(){return this->returnValue->getDataType();}
+    BasicSymbol* getReturnValue();
+    dataType getReturnType();
 };
 
 //call 
 class CallLLVM:public ControlOperationLLVM{
 public:
     FuncSymbol* function; // The function to call
-
     std::vector<BasicSymbol*> arguments; // The arguments to pass to the function
 
     std::string out_str() const override; // Output the LLVM IR string representation
-
     void setFunction(FuncSymbol* func); // Set the function to call
-
     void addArgument(BasicSymbol* arg); // Add an argument to the function call
-
     void addArguments(std::vector<BasicSymbol*> args);
-
     void setArguments(std::vector<BasicSymbol*>& args);
-
-    const std::vector<dataType>& getArgumentsType(){return this->function->getArgumentsType();};
-
-    template<typename ...Args>
-    void addArguments(Args... args){
-        (this->arguments.push_back(static_cast<BasicSymbol*>(args),...));
-    }
-
-    FuncSymbol* getFuncSymbol(){return this->function;}
-    const std::vector<BasicSymbol*>& getArguments() const{return this->arguments;}
-    const std::vector<dataType>& getArgumentsType() const{return this->function->getArgumentsType();}
+    const std::vector<dataType>& getArgumentsType();
+    FuncSymbol* getFuncSymbol();
+    const std::vector<BasicSymbol*>& getArguments() const;
+    const std::vector<dataType>& getArgumentsType() const;
 
 };
 
@@ -86,24 +67,17 @@ public:
 class SwitchLLVM:public ControlOperationLLVM{
 public:
     BasicSymbol* condition; // The condition for the switch
-
     std::vector<std::pair<BasicSymbol*,LabelSymbol*>> case_val_dest; //case values and destinations
-
     LabelSymbol* defaultCase; // The default case LLVM IR
 
     std::string out_str() const override; // Output the LLVM IR string representation
-
     void setCondition(BasicSymbol* cond); // Set the condition for the switch
-
     void addCase(BasicSymbol* case_val, LabelSymbol* case_des); // Add a case to the switch
-
     void addCase(std::vector<BasicSymbol*> case_val, std::vector<LabelSymbol*> case_des); // Add a case to the switch
-
     void setDefaultCase(LabelSymbol* defaultIR); // Set the default case LLVM IR
-
-    BasicSymbol* getCondition(){return this->condition;}
-    LabelSymbol* getDefaultCase(){return this->defaultCase;}
-    const std::vector<std::pair<BasicSymbol*,LabelSymbol*>> getCaseValAndDest()const{return this->case_val_dest;}
+    BasicSymbol* getCondition();
+    LabelSymbol* getDefaultCase();
+    const std::vector<std::pair<BasicSymbol*,LabelSymbol*>> getCaseValAndDest()const;
 };
 
 //dest_sym=phi dest_ty [src_sym1,label1],[src_sym2,label2]......
@@ -113,13 +87,11 @@ public:
     std::vector<std::pair<BasicSymbol*,LabelSymbol*>> vals_srcs;
 
     void addCase(BasicSymbol*src_sym,LabelSymbol*src_label);
-
     void addCase(std::vector<BasicSymbol*> src_sym,std::vector<LabelSymbol*>src_label);
-
     std::string out_str() const override;
-    BasicSymbol* getDestSymbol(){return this->dest_sym;}
-    const std::vector<std::pair<BasicSymbol*,LabelSymbol*>>getValAndSrc() const{return this->vals_srcs;}
-    dataType getDestType(){return this->dest_sym->getDataType();}
+    BasicSymbol* getDestSymbol();
+    const std::vector<std::pair<BasicSymbol*,LabelSymbol*>>getValAndSrc() const;
+    dataType getDestType();
 
 };
 
@@ -129,5 +101,5 @@ public:
     LabelSymbol* label;
 
     std::string out_str() const override;
-    LabelSymbol* getLabel(){return this->label;}
+    LabelSymbol* getLabel();
 };
