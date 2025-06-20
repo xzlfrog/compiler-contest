71521018ca882529f292bc58dd815fe134e2c41e
xzl_frog <114553117+xzlfrog@users.noreply.github.com>
2025-06-20 20:28:10 +0800

    Update BasicOperations.hpp

--- include/BasicOperations.hpp
+++ include/BasicOperations.hpp
@@ -3,9 +3,8 @@
 #include"sym.hpp"
 #include"llvm.hpp"
 
-//a=b op c;
+//a=op ty b c;
 class ArithmeticOperationLLVM : public LLVM{
-
 public:
     BasicSymbol* a;//result
     BasicSymbol* b;//operand 1
@@ -13,20 +12,14 @@ public:
     dataType ty;//b,c的数据类型，只有比较指令a，b，c的数据类型不同，此时a的类型位i1，其他时候三者数据类型相同
 
     void setLLVMType(LLVMtype type);
-
     std::string out_str() const override;//output the LLVM IR string representation
-    
     void setOperandA(BasicSymbol* operand);//set the result operand
-
     void setOperandB(BasicSymbol* operand);//set the first operand
-
     void setOperandC(BasicSymbol* operand);//set the second operand
-
     void setOperand(BasicSymbol* a,BasicSymbol* b,BasicSymbol *c);
-
-    BasicSymbol* getA(){return this->a;}
-    BasicSymbol* getB(){return this->b;}
-    BasicSymbol* getC(){return this->c;}
+    BasicSymbol* getA();
+    BasicSymbol* getB();
+    BasicSymbol* getC();
 
 };
 
