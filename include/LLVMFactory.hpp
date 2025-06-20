9d16861d1eb21209a01bc6def3a991c897f6606f
xzl_frog <114553117+xzlfrog@users.noreply.github.com>
2025-06-20 20:48:04 +0800

    Update LLVMFactory.hpp

--- include/LLVMFactory.hpp
+++ include/LLVMFactory.hpp
@@ -3,53 +3,35 @@
 #include"BasicOperations.hpp"
 #include"ControlOperations.hpp"
 #include"GlobalOperations.hpp"
-#include"LocalMemoryOperation.hpp"
-#include"TypeConversionOperation.hpp"
+#include"LocalMemoryOperations.hpp"
+#include"TypeConversionOperations.hpp"
 
 //LLVMfactory类中包含了一系列静态方法，这些静态方法用于得到我们需要的llvm类
 class LLVMfactory{
 public:
     static ArithmeticOperationLLVM* createBasicOperationLLVM(LLVMtype type,BasicSymbol*a,BasicSymbol*b,BasicSymbol*c);
-
     static ConditionalBranchLLVM* createConditionalBranchLLVM(BasicSymbol* cond, LabelSymbol* trueBranch, LabelSymbol* falseBranch);
-
     static UnconditionalBranchLLVM* createUnconditionalBranchLLVM(LabelSymbol* target);
-
     static ReturnLLVM* createReturnLLVM(BasicSymbol* returnValue);
-
-    //func必须来自于一个之前已经定义/声明过的函数对应的FuncSymbol*
-    template<typename ...Args>
-    static CallLLVM* createCallLLVM(FuncSymbol* func,Args... args){
-        CallLLVM* callLLVM=new CallLLVM();
-        callLLVM->addArguments(...args);
-        callLLVM->function=func;
-        callLLVM->llvmType=LLVMtype::call;
-        dataType arg_ty1,arg_ty2;
-        for(int i=0;i<callLLVM->arguments.size();i++)
-        {
-            arg_ty1=callLLVM->arguments[i]->data->getType();
-            arg_ty2=func->arguments[i];
-            if(arg_ty1!=arg_ty2)
-                throw std::invalid_argument("function parameters type not match");
-        }
-        return callLLVM;
-    }
-
+    static CallLLVM* createCallLLVM(FuncSymbol* func,std::vector<BasicSymbol*>& params);
     static SwitchLLVM* createSwitchLLVM(BasicSymbol* cond, LabelSymbol* defaultCase,std::vector<BasicSymbol*>case_val,std::vector<LabelSymbol*>case_dest);
-
     static PhiLLVM* createPhiLLVM(BasicSymbol* dest_sym,std::vector<BasicSymbol*> src_sym,std::vector<LabelSymbol*>src_label);
-
     static Label* createLableLLVM(LabelSymbol* label);
-
     static TypeConversionOperation* createTypeConversionOperation(LLVMtype type,BasicSymbol* src_sym,BasicSymbol* dest_sym);
-
     static AllocaNonArrayLLVM* createAllocaNonArrayLLVM(PointerSymbol* sym);
-
+    static AllocaArrayLLVM* createAllocaArrayLLVM(ArraySymbol* sym,dataType type,std::vector<int>&dimensions);
     static AllocaArrayLLVM* createAllocaArrayLLVM(ArraySymbol* sym);
-
     static LoadLLVM* createLoadLLVM(PointerSymbol* src_sym,BasicSymbol* dest_sym);
-
     static StoreLLVM* createStoreLLVM(BasicSymbol* src_sym,PointerSymbol* dest_sym);
-
-    static GetElementPtrLLVM* createGetElementPtrLLVM(ArraySymbol* ptrval,std::vector<dataType>type,std::vector<BasicSymbol*>idx);
+    static GetElementPtrLLVM* createGetElementPtrLLVM(BasicSymbol*dest_sym,ArraySymbol* ptrval,std::vector<dataType>type,std::vector<BasicSymbol*>idx);
+    static GlobalNonArrayVarDefination* createGlobalNonArrayVarDefination(PointerSymbol* dest_sym,BasicSymbol* src_sym);
+    static GlobalNonArrayVarDefination* createGlobalNonArrayVarDefination(PointerSymbol* dest_sym,initializer initMode);
+    static ConstantNonArrayVarDefination* createConstantNonArrayVarDefination(BasicSymbol* dest_sym,BasicSymbol* src_sym);
+    static ConstantNonArrayVarDefination* createConstantNonArrayVarDefination(BasicSymbol* dest_sym,initializer initMode);
+    static FuncDeclaration* createFuncDeclaration(FuncSymbol* func,std::vector<dataType>paramTypes,dataType returnType);
+    static FuncDeclaration* createFuncDeclaration(FuncSymbol* func,std::vector<dataType>paramTypes);
+    static FuncDeclaration* createFuncDeclaration(FuncSymbol* func);
+    static FuncDefination* createFuncDefination(FuncSymbol* func,std::vector<BasicSymbol*>param);
+    static FuncDefination* createFuncDefination(FuncSymbol* func,std::vector<dataType>paramTypes,dataType returnType,std::vector<BasicSymbol*>param);
+    static FuncDefination* createFuncDefination(FuncSymbol* func,std::vector<dataType>paramTypes,std::vector<BasicSymbol*>param);
 };
