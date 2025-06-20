68b8a3d72e66b1ff10cdfa5c0d738d33a40e6d34
xzl_frog <114553117+xzlfrog@users.noreply.github.com>
2025-06-20 20:49:47 +0800

    Update SymbolFactory.hpp

--- include/SymbolFactory.hpp
+++ include/SymbolFactory.hpp
@@ -3,13 +3,15 @@
 #include"sym.hpp"
 
 class SymbolFactory{
-    BasicSymbol* createBasicSymbol();
-
-    PointerSymbol* createPointerSymbol();
-
-    ArraySymbol* createArraySymbol();
-
-    LabelSymbol* createLabelSymbol();
-
-    FuncSymbol* createFuncSymbol();
+public:
+    static BasicSymbol* createBasicSymbol(std::string name,symType type=symType::sym_undefined,Data*data=nullptr);
+    static BasicSymbol* createTmpBasicSymbol(symType type=symType::sym_undefined,Data*data=nullptr);
+    static PointerSymbol* createPointerSymbol(std::string name,dataType pointedType=dataType::data_undefined);
+    static PointerSymbol* createPointerSymbol(std::string name,ValueVariant value,dataType pointedType=dataType::data_undefined);
+    static ArraySymbol* createArraySymbol(std::string name,dataType type=dataType::data_undefined);
+    static ArraySymbol* createArraySymbol(std::string name,std::vector<int>&dimensions,dataType type=dataType::data_undefined);
+    static LabelSymbol* createLabelSymbol(std::string name);
+    static LabelSymbol* createTmpLabelSymbol();
+    static FuncSymbol* createFuncSymbol(std::string name,std::vector<dataType>& paramTypes,dataType returnType=dataType::data_undefined);
+    static FuncSymbol* createFuncSymbol(std::string name,dataType returnType=dataType::data_undefined);
 };
