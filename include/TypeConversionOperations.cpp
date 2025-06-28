6e23b8c19abbdf20d227b14104f38569cf49e8d7
xzl_frog <114553117+xzlfrog@users.noreply.github.com>
2025-06-28 22:06:48 +0800

    Delete include/TypeConversionOperations.cpp

--- include/TypeConversionOperations.cpp
+++
@@ -1,9 +0,0 @@
-#include"../include/TypeConversionOperations.hpp"
-
-//TypeConversionOperation::
-BasicSymbol* TypeConversionOperation::getSrcSymbol(){return this->src_sym;}
-BasicSymbol* TypeConversionOperation::getDestSymbol(){return this->dest_sym;}
-dataType TypeConversionOperation::getSrcType(){return this->src_sym->getDataType();}
-dataType TypeConversionOperation::getDestType(){return this->dest_sym->getDataType();}
-
-std::string TypeConversionOperation::out_str() const {}
