4d8f44c615c48ee63daa5090298e252c5cea060f
illAlwayswatching <2104962413@qq.com>
2025-07-22 22:26:28 +0800

    debug后端1.1 如何正确获取dataType

---
+++ test/test1.s
@@ -0,0 +1,8 @@
+.module start
+.endmodule
+.module start
+@func.getMaxNum :
+    ;Function prologue
+    stp x29, x30, [sp, #-16]!  // Save FP and LR
+    mov x29, sp                                    // Set new FP
+
