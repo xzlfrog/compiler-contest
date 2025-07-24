0647c59152a06917c8c3a5559585d09a12c19ab8
编编编 <t202510614206372@eduxiji.net>
2025-07-24 01:31:12 +0000

    Upload New File

---
+++ src/UnaryOperation.cpp
@@ -0,0 +1,23 @@
+#include"../include/UnaryOperation.hpp"
+
+dataType UnaryOperationLLVM::getType(){
+    return this->dest_sym->getDataType();
+}
+
+std::string UnaryOperationLLVM::out_str() const{
+    std::string res;
+    res=this->dest_sym->getName();
+    switch (this->llvmType)
+    {
+        case LLVMtype::llvm_neg:
+            res+=" = neg ";
+            break;
+        case LLVMtype::llvm_fneg:
+            res+=" = fneg ";
+            break;
+        default:
+            break;
+    }
+    res+=Data::getTypeStr(this->dest_sym->getDataType())+" "+getSymOut(this->src_sym);
+    return res;
+}
