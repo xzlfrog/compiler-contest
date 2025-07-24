ba12f4005a6c609ccd095541b6d6369ad6a4ed69
编编编 <t202510614206372@eduxiji.net>
2025-07-24 01:30:28 +0000

    Upload New File

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include"../include/frontend/frontendTool.hpp"
+#include"frontend/sysy.y.hpp"
+#include"../include/backend/out_arm.hpp"
+#include<stdio.h>
+
+int scope;
+ModuleList* module_list;
+std::vector<std::unordered_map<std::string,int>> variable_rename_table;
+std::vector<std::unordered_map<std::string,Symbol*>> variable_table;
+std::unordered_map<std::string,FuncSymbol*>func_table;
+std::unordered_map<std::string,BasicSymbol*> pointer_to_var;
+std::stack<LabelSymbol*> break_st,continue_st;
+extern FILE* yyin;
+extern std::ofstream outputArmFile;
+std::queue<Expression*>assign_queue;
+std::stack<int>array_initial;
+int array_init_idx;
+
+//compiler -S -o testcase.s testcase.sy
+int main(int argc,char* argv[]){
+    //if(argc != 2) printf("usage: %s filename\n", argv[0]);
+    FILE* inputFile=fopen(argv[4],"r");
+    if (!inputFile) {
+        printf("Error: Unable to open input file %s\n", argv[4]);
+        return 1;
+    }
+    std::string inputFileName = argv[4];
+    std::string outputFileName = inputFileName.substr(0, inputFileName.find_last_of('.')) + ".s";
+    outputArmFile.open(outputFileName, std::ios::out | std::ios::trunc);
+    if (!outputArmFile.is_open()) {
+        printf("Error: Unable to create output file %s\n", outputFileName.c_str ());
+        return 1;
+    }
+    
+    yyin = inputFile;
+    begin_parser();
+    yyparse();
+    out_arm(outputFileName,module_list);
+
+    return 0;
+}
