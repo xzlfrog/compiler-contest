b85923edd4ba1a5a935be22d7885e2f0e8c90b2d
illAlwayswatching <2104962413@qq.com>
2025-08-01 19:31:47 +0800

    修复了浮点数常数的情况

--- include/backend/StaAlloca.hpp
+++ include/backend/StaAlloca.hpp
@@ -25,7 +25,7 @@ class StackAllocator {
   // 变量名 -> 栈偏移
         int stack_currentOffset = 0;    // 栈帧指针；
         std::unordered_map<std::string, std::string> RegVar_StackVar; //寄存器 栈帧 映射
-        std::unordered_set<std::string> Tmp_StackAddress_InReg; //前变量 后 寄存器 ；getelem时候 临时存一下  
+        std::unordered_set<std::string> Tmp_StackAddress_InReg; //前变量 后 寄存器（统一为X8） ；getelem时候 临时存一下  
 
         std::set<std::string> usedFloatRegisters;
         std::set<std::string> usedRegisters;
