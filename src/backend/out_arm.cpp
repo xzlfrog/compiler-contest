088935a5e4112a0b1aa10c293e55ddb540c3cdd3
123 <13@qq>
2025-08-02 17:36:15 +0800

    ddd

--- src/backend/out_arm.cpp
+++ src/backend/out_arm.cpp
@@ -1352,13 +1352,10 @@ void TypeConversionOperation::out_arm_str()  {
     }
 
     switch (this->llvmType) {
-        case llvm_trunc:
-            VarSymbol* tmp = SymbolFactory::createTmpVarSymbolWithScope(dataType::i32, 1);
-            std::string tmp_tmp_str = out_Arm.DispatchReg(tmp);
-            OutArm::outString("\tMOV " + tmp_tmp_str + ", #1" );
-            OutArm::outString("\tAND " + src_str + ", " + tmp_tmp_str );
+        case llvm_trunc:{
             OutArm::outString("\tMOV " + dest_str + ", " + src_str);
             break;
+        }
         case zext:
             OutArm::outString("\tMOV " + dest_str + ", " + src_str);
             break;
