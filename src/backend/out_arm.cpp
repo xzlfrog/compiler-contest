9cff51953e6f2febb2bef40bfd2bdb0a944f442d
illAlwayswatching <2104962413@qq.com>
2025-08-02 09:23:24 +0800

    测试内存溢出原因2 栈帧初始化

--- src/backend/out_arm.cpp
+++ src/backend/out_arm.cpp
@@ -879,31 +879,31 @@ void AllocaArrayLLVM::out_arm_str()  {
     //int datasize = OutArm::getDataSize(this->array);
     int size = out_Arm.stackAllocator.allocateArray( 4 ,this->getDimensions(),this->array->getName());
 
-    std::vector<int> dims = this->getDimensions();
-    int totaltimes = 1;
-    for (int dim : dims) {
-        totaltimes *= dim;
-    }
+    // std::vector<int> dims = this->getDimensions();
+    // int totaltimes = 1;
+    // for (int dim : dims) {
+    //     totaltimes *= dim;
+    // }
 
-    int first_address = out_Arm.stackAllocator.getOffset(this->getArray()->getName());
-    OutArm::outString("\tSTR WZR, [SP, #" + std::to_string(first_address) + "]!");
-    out_Arm.stackAllocator.stack_currentOffset += first_address;
-    totaltimes--;
+    // int first_address = out_Arm.stackAllocator.getOffset(this->getArray()->getName());
+    // OutArm::outString("\tSTR WZR, [SP, #" + std::to_string(first_address) + "]!");
+    // out_Arm.stackAllocator.stack_currentOffset += first_address;
+    // totaltimes--;
 
-    int offset = 4;
-    int side_offset = 240;
+    // int offset = 4;
+    // int side_offset = 240;
 
-    for (int i = 0; i < totaltimes; ++i) {
-        if (offset == side_offset) {
-            OutArm::outString("\tSTR WZR, [SP, #" + std::to_string(offset) + "]!");
-            out_Arm.stackAllocator.stack_currentOffset += offset;
-            offset = 4;//重置
-        } else {
-            OutArm::outString("\tSTR WZR, [SP, #" + std::to_string(offset) + "]");
-        }
+    // for (int i = 0; i < totaltimes; ++i) {
+    //     if (offset == side_offset) {
+    //         OutArm::outString("\tSTR WZR, [SP, #" + std::to_string(offset) + "]!");
+    //         out_Arm.stackAllocator.stack_currentOffset += offset;
+    //         offset = 4;//重置
+    //     } else {
+    //         OutArm::outString("\tSTR WZR, [SP, #" + std::to_string(offset) + "]");
+    //     }
 
-        offset+=4;
-    }
+    //     offset+=4;
+    // }
 
 }
 
